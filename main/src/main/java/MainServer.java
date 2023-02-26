9da6d23fe7e928734334befe5e5aa6a3c71f2304
seer <kechin13@gmail.com>
2023-02-26 15:17:33 -0500

    r

---
+++ main/src/main/java/MainServer.java
@@ -0,0 +1,13 @@
+import lombok.Generated;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class MainServer {
+    @Generated
+    public static void main(String[] args) {
+        SpringApplication.run(MainServer.class, args);
+
+    }
+
+}
